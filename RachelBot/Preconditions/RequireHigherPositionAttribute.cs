1d3f5b4bdef0f4aa9a2775c332f828a4a6193ae6
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-02-13 22:18:21 +0100

    Added few preconditions

---
+++ RachelBot/Preconditions/RequireHigherPositionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace RachelBot.Preconditions
+{
+    public class RequireHigherPositionAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            SocketGuildUser user = (SocketGuildUser)context.User;
+            SocketGuildUser bot = (SocketGuildUser)context.Guild.GetUserAsync(context.Client.CurrentUser.Id).GetAwaiter().GetResult();
+
+            if (bot.Hierarchy > user.Hierarchy)
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+            else
+            {
+                return Task.FromResult(PreconditionResult.FromError($"Insufficient Permissions"));
+            }
+        }
+    }
+}
---
+++ RachelBot/Preconditions/RequireStaffAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using RachelBot.Core.Configs;
+using RachelBot.Services.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.WebSocket;
+
+namespace RachelBot.Preconditions
+{
+    public class RequireStaffAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            GuildConfig config = new GuildConfigs(context.Guild.Id, services.GetService<IStorageService>()).GetGuildConfig();
+
+            foreach (ulong roleId in config.StaffRoleIds)
+            {
+                if ((context.User as SocketGuildUser).Roles.SingleOrDefault(r => r.Id == roleId) != null)
+                {
+                    return Task.FromResult(PreconditionResult.FromSuccess());
+                }
+            }
+
+            return Task.FromResult(PreconditionResult.FromError($"You are not server staff"));
+        }
+    }
+}
