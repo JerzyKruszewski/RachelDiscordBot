e67af061f133e3cc665080c6e200c715662867b3
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-02-13 13:10:27 +0100

    Implemented discord bot template

---
+++ RachelBot/EventHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Addons.Interactive;
+
+namespace RachelBot
+{
+    public class EventHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+
+            (_service as IDisposable)?.Dispose();
+            _service = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(new InteractiveService(_client))
+                .BuildServiceProvider();
+
+            var cmdConfig = new CommandServiceConfig
+            {
+                DefaultRunMode = RunMode.Async
+            };
+
+            (_commands as IDisposable)?.Dispose();
+            _commands = new CommandService(cmdConfig);
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _service);
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            try
+            {
+                if (arg.Channel is IPrivateChannel)
+                {
+                    return;
+                }
+
+                SocketUserMessage msg = arg as SocketUserMessage;
+                if (msg == null) return;
+                SocketCommandContext context = new SocketCommandContext(_client, msg);
+
+                int argPos = 0;
+                if (msg.HasStringPrefix(ConfigurationManager.AppSettings["Prefix"], ref argPos)
+                    || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+                {
+                    var result = await _commands.ExecuteAsync(context, argPos, _service);
+                    if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    {
+                        Console.WriteLine(result.ErrorReason);
+                        await context.Channel.SendMessageAsync(result.ErrorReason);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
--- RachelBot/Program.cs
+++ RachelBot/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace RachelBot
 {
     internal class Program
     {
-        private static void Main()
+        private DiscordSocketClient _client;
+        private EventHandler _handler;
+
+        private static void Main() => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private async Task RunBotAsync()
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["Token"]))
+                {
+                    return;
+                }
+
+                _client?.Dispose();
+                _client = new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Verbose,
+                    MessageCacheSize = 0, 
+                    AlwaysDownloadUsers = true
+                });
+
+                await InitializationClient();
+                await InitializationLogs();
+
+                await Task.Delay(-1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+
+        private async Task InitializationClient()
+        {
+            await _client.SetGameAsync(ConfigurationManager.AppSettings["Game"]);
+            await LoginAsync();
+            await HandlerInitialize();
+        }
+
+        private async Task LoginAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, ConfigurationManager.AppSettings["Token"]);
+            await _client.StartAsync();
+        }
+
+        private async Task HandlerInitialize()
+        {
+            _handler = new EventHandler();
+            await _handler.InitializeAsync(_client);
+        }
+
+        private Task InitializationLogs()
+        {
+            _client.Log += BotLog;
+
+            return Task.CompletedTask;
+        }
+
+        private Task BotLog(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
+            return Task.CompletedTask;
         }
     }
 }
