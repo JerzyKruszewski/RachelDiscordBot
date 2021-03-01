83b52f38e90405c7ade5c0f7601b911cc1fbb9e3
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-03-01 14:27:25 +0100

    Added basic feedback for configuration

--- RachelBot/Commands/ConfigurationCommands.cs
+++ RachelBot/Commands/ConfigurationCommands.cs
@@ -9,6 +9,7 @@ using Discord.WebSocket;
 using Discord.Addons.Interactive;
 using RachelBot.Core.Configs;
 using RachelBot.Services.Storage;
+using RachelBot.Lang;
 
 namespace RachelBot.Commands
 {
@@ -26,14 +27,22 @@ namespace RachelBot.Commands
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeGuildPrefix(string prefix)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeGuildPrefix(prefix);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeGuildPrefix(prefix);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeGuildLanguage")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeGuildLanguage(string languageIso)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeGuildLanguage(languageIso);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeGuildLanguage(languageIso);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("AddStaffRoles")]
@@ -47,7 +56,11 @@ namespace RachelBot.Commands
                 list.Add(role.Id);
             }
 
-            new GuildConfigs(Context.Guild.Id, _storage).AddStaffRoles(list);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.AddStaffRoles(list);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeStaffRoles")]
@@ -61,112 +74,176 @@ namespace RachelBot.Commands
                 list.Add(role.Id);
             }
 
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeStaffRoles(list);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeStaffRoles(list);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeGuildModerationChannel")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeGuildModerationChannel(ITextChannel channel)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeGuildModerationChannel(channel.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeGuildModerationChannel(channel.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeUsersJoiningChannel")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeUsersJoiningChannel(ITextChannel channel)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeUsersJoiningChannel(channel.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeUsersJoiningChannel(channel.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeWelcomeMessage")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeWelcomeMessage([Remainder]string message)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeWelcomeMessage(message);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeWelcomeMessage(message);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeUsersLeftChannel")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeUsersLeftChannel(ITextChannel channel)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeUsersLeftChannel(channel.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeUsersLeftChannel(channel.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeUserLeftMessage")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeUserLeftMessage([Remainder]string message)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeUserLeftMessage(message);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeUserLeftMessage(message);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangePunishmentRole")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangePunishmentRole(IRole role)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangePunishmentRole(role.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangePunishmentRole(role.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangePunishmentChannel")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangePunishmentChannel(ITextChannel channel)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangePunishmentChannel(channel.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangePunishmentChannel(channel.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("TogglePointSystemWarns")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task TogglePointSystemWarns(bool toggle)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).TogglePointSystemWarns(toggle);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.TogglePointSystemWarns(toggle);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeWarnDuration")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeWarnDuration(uint duration)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeWarnDuration(duration);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeWarnDuration(duration);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeWarnCountTillBan")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeWarnCountTillBan(uint warnCount)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeWarnCountTillBan(warnCount);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeWarnCountTillBan(warnCount);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeWarnCountTillPunishment")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeWarnCountTillPunishment(uint warnCount)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeWarnCountTillPunishment(warnCount);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeWarnCountTillPunishment(warnCount);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeWarnPointsTillBan")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeWarnPointsTillBan(uint warnCount)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeWarnPointsTillBan(warnCount);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeWarnPointsTillBan(warnCount);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeWarnPointsTillPunishment")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeWarnPointsTillPunishment(uint warnCount)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeWarnPointsTillPunishment(warnCount);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeWarnPointsTillPunishment(warnCount);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeAnnouncementChannel")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeAnnouncementChannel(ITextChannel channel)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeAnnouncementChannel(channel.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeAnnouncementChannel(channel.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
 
         [Command("ChangeToSChannel")]
         [RequireUserPermission(GuildPermission.Administrator)]
         public async Task ChangeToSChannel(ITextChannel channel)
         {
-            new GuildConfigs(Context.Guild.Id, _storage).ChangeToSChannel(channel.Id);
+            GuildConfigs configs = new GuildConfigs(Context.Guild.Id, _storage);
+
+            configs.ChangeToSChannel(channel.Id);
+
+            await Context.Channel.SendMessageAsync(new AlertsHandler(configs.GetGuildConfig()).GetAlert("SUCCESS"));
         }
     }
 }
