7e1acc5afd621407b7f1f282fac07a307e9a6deb
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-03-05 18:46:12 +0100

    Added owner commands

---
+++ RachelBot/Commands/OwnerCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Addons.Interactive;
+using RachelBot.Core.Configs;
+using RachelBot.Services.Storage;
+using RachelBot.Preconditions;
+using RachelBot.Core.UserAccounts;
+using RachelBot.Utils;
+using RachelBot.Core.LevelingSystem;
+using RachelBot.Lang;
+
+namespace RachelBot.Commands
+{
+    public class OwnerCommands : InteractiveBase<SocketCommandContext>
+    {
+        [Command("Guilds")]
+        [RequireOwner]
+        public async Task GetGuilds()
+        {
+            await Context.Channel.SendMessageAsync($"Guilds: {Context.Client.Guilds.Count}");
+        }
+    }
+}
